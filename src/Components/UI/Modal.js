7a3d506b59b7f920bf09ad33b452d670e2d262f6
Rohan Pawar <rohan22pawar@gmail.com>
2022-05-08 18:27:29 +0530

    added cart logic and bump effect

--- src/Components/UI/Modal.js
+++ src/Components/UI/Modal.js
@@ -1,9 +1,10 @@
-import { Fragment } from "react";
-import ReactDom from "react-dom";
-import classes from "./Modal.module.css";
+import { Fragment } from 'react';
+import ReactDOM from 'react-dom';
+
+import classes from './Modal.module.css';
 
 const Backdrop = (props) => {
-  return <div className={classes.backdrop} onClick={props.onClose} />;
+  return <div className={classes.backdrop} onClick={props.onClose}/>;
 };
 
 const ModalOverlay = (props) => {
@@ -14,13 +15,13 @@ const ModalOverlay = (props) => {
   );
 };
 
-const portalElement = document.getElementById("overlays");
+const portalElement = document.getElementById('overlays');
 
 const Modal = (props) => {
   return (
     <Fragment>
-      {ReactDom.createPortal(<Backdrop onClose={props.onClose} />, portalElement)}
-      {ReactDom.createPortal(
+      {ReactDOM.createPortal(<Backdrop onClose={props.onClose} />, portalElement)}
+      {ReactDOM.createPortal(
         <ModalOverlay>{props.children}</ModalOverlay>,
         portalElement
       )}
